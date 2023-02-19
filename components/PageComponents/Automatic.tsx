6636b08aa689581c4c0cfd1e474c252b932212ff
supersenior017 <hoj256274@gmail.com>
2023-02-19 20:55:46 +0900

    changed trading section

--- components/PageComponents/Automatic.tsx
+++ components/PageComponents/Automatic.tsx
@@ -11,13 +11,25 @@ export default function Automatic() {
                 <p>Automate ALL your Trades...</p>
                 <p>Even the Manual Ones!</p>
             </div>
-            <div className='relative'>
+            <div className='relative hidden md:block'>
                 <div className='text-center absolute z-50 w-full h-full flex'>
                     <div className='max-w-[700px] flex justify-center flex-col m-auto text-slate-300 text-xl my-8'>
                         There is no such thing as risk-free trading. We do not create investment strategies and do not provide investment advice. These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard.
                     </div>
                 </div>
-                <div className='blur-sm' style={{ width: '100%' }}>
+                <div className='blur-[2px]' style={{ width: '100%' }}>
+                    <video autoPlay loop>
+                        <source src="/video/trading.mp4" />
+                    </video>
+                </div>
+            </div>
+            <div className='block md:hidden'>
+                <div className='text-center z-50 w-full h-full flex'>
+                    <div className='max-w-[700px] flex justify-center flex-col m-auto text-slate-300 text-xl my-8'>
+                        There is no such thing as risk-free trading. We do not create investment strategies and do not provide investment advice. These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard.
+                    </div>
+                </div>
+                <div className='' style={{ width: '100%' }}>
                     <video autoPlay loop>
                         <source src="/video/trading.mp4" />
                     </video>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ChatGPT | Home</title>
+        <title>Automated Trader</title>
         <meta name="description" content="You can enjoy high quality, artificial intelligence chatbots." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
