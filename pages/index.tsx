d472beff3550f642850ecea97083f97d1a7a8de4
supersenior017 <hoj256274@gmail.com>
2023-02-19 20:57:43 +0900

    changed logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Automated Trader</title>
         <meta name="description" content="You can enjoy high quality, artificial intelligence chatbots." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <Homepage />
     </>
